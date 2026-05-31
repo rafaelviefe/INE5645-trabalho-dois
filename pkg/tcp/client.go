869503ab912cfee6fa8083dfd0e97aaccdefd7e9
UrielJaloto <urieljaloto@gmail.com>
2026-05-31 17:31:54 -0300

    Add timeouts to TCP client connection

--- pkg/tcp/client.go
+++ pkg/tcp/client.go
@@ -2,14 +2,17 @@ package tcp
 
 import (
 	"net"
+	"time"
 )
 
 func SendRequest(address string, payload []byte) ([]byte, error) {
-	conn, err := net.Dial("tcp", address)
+	conn, err := net.DialTimeout("tcp", address, 3*time.Second)
 	if err != nil {
 		return nil, err
 	}
 	defer conn.Close()
+
+	conn.SetDeadline(time.Now().Add(5 * time.Second))
 
 	if err := WriteMessage(conn, payload); err != nil {
 		return nil, err
